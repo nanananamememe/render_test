8520d45beb7f280e0ab79fad7e20365e13bc8207
kamashige <kamashige@n-create.co.jp>
2024-01-22 21:35:36 +0900

    second

---
+++ public/sw.js
@@ -0,0 +1,25 @@
+self.addEventListener('push', evt => {
+    const data = evt.data.json();
+    console.log(data);
+    const title = data.title;
+    const options = {
+        body: data.body,
+        icon: data.icon,
+        data: data.data
+    }
+    evt.waitUntil(self.registration.showNotification(title, options));
+});
+self.addEventListener('notificationclick', evt => {
+  evt.notification.close();
+  var url = "/"
+  if (evt.notification.data.url) {
+    url = evt.notification.data.url
+  }
+  evt.waitUntil(
+    clients.matchAll({type: 'window'}).then(function() {
+      if(clients.openWindow) {
+        return clients.openWindow(url)
+      }
+    })
+  )
+});
